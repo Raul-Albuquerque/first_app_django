3dee1842922b2b9c10d845aa59c323860cea5099
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2024-03-05 22:53:47 -0300

    feat: CRUD complete

--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,8 @@
-from django.views.generic import ListView, CreateView, UpdateView
+from datetime import date
+
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, View
 from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404, redirect
 
 from .models import Todo
 
@@ -19,3 +22,16 @@ class TodoUpdateView(UpdateView):
     model = Todo
     fields = ["title", "deadline"]
     success_url = reverse_lazy("todo_list")
+
+
+class TodoDeleteView(DeleteView):
+    model = Todo
+    success_url = reverse_lazy("todo_list")
+
+
+class TodoCompleteView(View):
+    def get(self, request, pk):
+        todo = get_object_or_404(Todo, pk=pk)
+        todo.finished_at = date.today()
+        todo.save()
+        return redirect("todo_list")
